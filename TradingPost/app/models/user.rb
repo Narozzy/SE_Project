845478278a157a7903b05e57442a88587b025af3
Noah Rozelle <nrozelle@uncc.edu>
2019-10-20 15:34:59 -0400

    Added validation to the user model, looking into creation of a validator class

---
+++ TradingPost/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    # This creates a one-to-many relationship to the listings model
+    has_many :listings
+    # Validations for account creation, may want to create a separate class for validation
+    validates :email, confirmation: true,
+                      inclusion: { in: %w(edu), message: 'Must be a student email to register an account.'},
+                      uniqueness: true
+    validates :email_confirmation, presence: true
+end
