e861c9a8e27d18cae091a4db94768c0564099eb7
Noah Rozelle <nrozelle@uncc.edu>
2019-10-20 15:45:27 -0400

    added validation to model/listing.rb

--- TradingPost/app/models/listing.rb
+++ TradingPost/app/models/listing.rb
@@ -1,3 +1,14 @@
 class Listing < ApplicationRecord
+    # Creates a master-detail relationship from this listing to an account
     belongs_to :user
+
+    # Listing validation
+    validates :title, presence: true, length: { minimum: 5, message: 'Must be 5 or more characters' }
+    validates :isbn, presence: true, if: -> :is_book?
+    validates :offer_type, presence: { message: 'List what type of item you are looking to post' }, 
+    
+
+    def is_book?
+        offer_type == 'book'
+    end
 end
