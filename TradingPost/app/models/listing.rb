dc18e97d7851f27312c8506769f789492378bba2
Noah Rozelle <nrozelle@uncc.edu>
2019-10-26 18:58:25 -0400

    Added validatio to user, added scripts to modify looks interactively of elements such that the user will know in real time if there are fields with invalid inputs, notably the email validation

--- TradingPost/app/models/listing.rb
+++ TradingPost/app/models/listing.rb
@@ -1,2 +1,8 @@
 class Listing < ApplicationRecord
+    validates :isbn, presence: { if: :is_book?, :message => 'ISBN must be defined for book listings' }
+    validates :title, presence: :true
+
+    def is_book?
+        return self.product_type == 'book'
+    end
 end
--- TradingPost/config/routes.rb
+++ TradingPost/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :listings
   resources :users
+  resources :welcome
   
   get 'users/index'
   root 'users#index'
