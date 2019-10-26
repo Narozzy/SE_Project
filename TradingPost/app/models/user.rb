c87bbeeec0c25459fb0d773cee34d44d2191c108
Noah Rozelle <nrozelle@uncc.edu>
2019-10-26 19:19:54 -0400

    Got email validation working through the use of regular expression in the User model class

--- TradingPost/app/models/user.rb
+++ TradingPost/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, format: { with: /\A([a-zA-Z]|\d)+@([a-zA-Z]|\d)+(.edu)\z/i, message: "Your email must contain \".edu\""}
 end
