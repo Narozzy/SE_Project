290d2ce1a7d9278471f5ae9231e9ad4895d17760
Noah Rozelle <nrozelle@uncc.edu>
2019-10-31 17:21:26 -0400

    New branch, working from here to configure the listings page and work on the backend

--- TradingPost/config/routes.rb
+++ TradingPost/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/index'
+  get 'index/index'
   resources :home
   root 'home#index'
 
