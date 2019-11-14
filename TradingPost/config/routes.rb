824f3a6d0c270f5641d808e436a5e7b616ba9827
Noah Rozelle <nrozelle@uncc.edu>
2019-11-14 08:02:26 -0500

    Getting page looking more like a separate page than index, need to implement query and get reference to the user object for the a listing shown

--- TradingPost/config/routes.rb
+++ TradingPost/config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/index'
   get 'index/index'
+  get 'users/index'
+  get 'listings/index'
   resources :home
+  resources :users
   root 'home#index'
 
-  
+  resources :listings do
+    resources :users
+  end
 end
